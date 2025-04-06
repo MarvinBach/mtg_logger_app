10419d2e1915ce3b00678ab5f779ab380586098c
Marvin <marbachmann90@gmail.com>
2025-04-06 21:08:12 +0200

    st_supabase_connection test

--- db.py
+++ db.py
@@ -9,17 +9,32 @@ from models import Base
 #SUPABASE_URL = os.getenv("SUPABASE_URL")
 #SUPABASE_PASSWORD = os.getenv("SUPABASE_PASSWORD")
 
-SUPABASE_URL = st.secrets["supabase"]["url"]
-SUPABASE_PASSWORD = st.secrets["supabase"]["password"]
+import streamlit as st
+from st_supabase_connection import SupabaseConnection
+
+# Initialize connection.
+conn = st.connection("supabase",type=SupabaseConnection)
+
+# Perform query.
+rows = conn.query("*", table="mytable", ttl="10m").execute()
+
+# Print results.
+for row in rows.data:
+    st.write(f"{row['name']} has a :{row['pet']}:")
+
+
+#
+#SUPABASE_URL = st.secrets["supabase"]["url"]
+#SUPABASE_PASSWORD = st.secrets["supabase"]["password"]
 
 #if "db." not in SUPABASE_URL:
 #    SUPABASE_URL = f"db.{SUPABASE_URL}"
 
-DATABASE_URL = f"postgresql://postgres:{SUPABASE_PASSWORD}@{SUPABASE_URL}:5432/postgres"
+#DATABASE_URL = f"postgresql://postgres:{SUPABASE_PASSWORD}@{SUPABASE_URL}:5432/postgres"
 
 
-engine = create_engine(DATABASE_URL, echo=True)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+#engine = create_engine(DATABASE_URL, echo=True)
+#SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-def init_db():
-    Base.metadata.create_all(bind=engine)
+#def init_db():
+#    Base.metadata.create_all(bind=engine)
