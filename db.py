bb231d525ff756f2fd4dc65c4b13bb6b4f1bdeaa
Marvin <marbachmann90@gmail.com>
2025-04-06 20:00:17 +0200

    updated db to use streamlit credentials

--- db.py
+++ db.py
@@ -1,4 +1,5 @@
-import os
+# import os
+import streamlit as st
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from models import Base
@@ -6,8 +7,11 @@ from models import Base
 from dotenv import load_dotenv
 load_dotenv(".env")
 
-SUPABASE_URL = os.getenv("SUPABASE_URL")
-SUPABASE_PASSWORD = os.getenv("SUPABASE_PASSWORD")
+#SUPABASE_URL = os.getenv("SUPABASE_URL")
+#SUPABASE_PASSWORD = os.getenv("SUPABASE_PASSWORD")
+
+SUPABASE_URL = st.secrets["supabase"]["url"]
+SUPABASE_PASSWORD = st.secrets["supabase"]["password"]
 
 DATABASE_URL = f"postgresql://postgres:{SUPABASE_PASSWORD}@db.{SUPABASE_URL}:5432/postgres"
 
