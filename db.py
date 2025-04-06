76e2580ee81c39c6d0025f819a3c932f3cee57ba
Marvin <marbachmann90@gmail.com>
2025-04-06 20:13:42 +0200

    updated db issue

--- db.py
+++ db.py
@@ -1,19 +1,21 @@
-# import os
+import os
 import streamlit as st
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from models import Base
 
-# from dotenv import load_dotenv
-# load_dotenv(".env")
-
+#from dotenv import load_dotenv
+#load_dotenv(".env")
 #SUPABASE_URL = os.getenv("SUPABASE_URL")
 #SUPABASE_PASSWORD = os.getenv("SUPABASE_PASSWORD")
 
 SUPABASE_URL = st.secrets["supabase"]["url"]
 SUPABASE_PASSWORD = st.secrets["supabase"]["password"]
 
-DATABASE_URL = f"postgresql://postgres:{SUPABASE_PASSWORD}@db.{SUPABASE_URL}:5432/postgres"
+if "db." not in SUPABASE_URL:
+    SUPABASE_URL = f"db.{SUPABASE_URL}"
+
+DATABASE_URL = f"postgresql://postgres:{SUPABASE_PASSWORD}@{SUPABASE_URL}:5432/postgres"
 
 
 engine = create_engine(DATABASE_URL, echo=True)
