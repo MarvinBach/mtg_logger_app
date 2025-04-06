5f0340906a11da38073286a9c9f88c5d5c9a9df8
Marvin <marbachmann90@gmail.com>
2025-04-06 21:13:58 +0200

    add app_test.py

--- db.py
+++ db.py
@@ -1,4 +1,4 @@
-# import os
+#import os
 import streamlit as st
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -9,32 +9,17 @@ from models import Base
 #SUPABASE_URL = os.getenv("SUPABASE_URL")
 #SUPABASE_PASSWORD = os.getenv("SUPABASE_PASSWORD")
 
-import streamlit as st
-from st_supabase_connection import SupabaseConnection
+SUPABASE_URL = st.secrets["supabase"]["url"]
+SUPABASE_PASSWORD = st.secrets["supabase"]["password"]
 
-# Initialize connection.
-conn = st.connection("supabase",type=SupabaseConnection)
+if "db." not in SUPABASE_URL:
+    SUPABASE_URL = f"db.{SUPABASE_URL}"
 
-# Perform query.
-rows = conn.query("*", table="mytable", ttl="10m").execute()
-
-# Print results.
-for row in rows.data:
-    st.write(f"{row['name']} has a :{row['pet']}:")
+DATABASE_URL = f"postgresql://postgres:{SUPABASE_PASSWORD}@{SUPABASE_URL}:5432/postgres"
 
 
-#
-#SUPABASE_URL = st.secrets["supabase"]["url"]
-#SUPABASE_PASSWORD = st.secrets["supabase"]["password"]
+engine = create_engine(DATABASE_URL, echo=True)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-#if "db." not in SUPABASE_URL:
-#    SUPABASE_URL = f"db.{SUPABASE_URL}"
-
-#DATABASE_URL = f"postgresql://postgres:{SUPABASE_PASSWORD}@{SUPABASE_URL}:5432/postgres"
-
-
-#engine = create_engine(DATABASE_URL, echo=True)
-#SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-#def init_db():
-#    Base.metadata.create_all(bind=engine)
+def init_db():
+    Base.metadata.create_all(bind=engine)
