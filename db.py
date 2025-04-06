31a1cbe91c571ba44fc6e3620948ac0625ea8014
Marvin <marbachmann90@gmail.com>
2025-04-06 22:06:33 +0200

    add test app

--- db.py
+++ db.py
@@ -1,30 +1,29 @@
-#import os
-import streamlit as st
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from models import Base
 
-#from dotenv import load_dotenv
-#load_dotenv(".env")
-#SUPABASE_URL = os.getenv("SUPABASE_URL")
-#SUPABASE_PASSWORD = os.getenv("SUPABASE_PASSWORD")
+from dotenv import load_dotenv
+load_dotenv(".env")
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_PASSWORD = os.getenv("SUPABASE_PASSWORD")
 
-#SUPABASE_URL = st.secrets["supabase"]["url"]
-#SUPABASE_PASSWORD = st.secrets["supabase"]["password"]
+if "db." not in SUPABASE_URL:
+    SUPABASE_URL = f"db.{SUPABASE_URL}"
 
-#if "db." not in SUPABASE_URL:
-#    SUPABASE_URL = f"db.{SUPABASE_URL}"
-
-#DATABASE_URL = f"postgresql://postgres:{SUPABASE_PASSWORD}@{SUPABASE_URL}:5432/postgres"
-
-db_url = st.secrets["SUPABASE_FULL_URL"]
+DATABASE_URL = f"postgresql://postgres:{SUPABASE_PASSWORD}@{SUPABASE_URL}:5432/postgres"
 
 
-
-#engine = create_engine(DATABASE_URL, echo=True)
-engine = create_engine(db_url)
+engine = create_engine(DATABASE_URL, echo=True)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 def init_db():
     Base.metadata.create_all(bind=engine)
+
+
+
+#-------------
+#import streamlit as st
+#SUPABASE_URL = st.secrets["supabase"]["url"]
+#SUPABASE_PASSWORD = st.secrets["supabase"]["password"]
