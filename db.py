a0fe89ff481725cc6f7791954dd332b15d957076
Marvin <marbachmann90@gmail.com>
2025-04-06 21:30:08 +0200

    games test

--- db.py
+++ db.py
@@ -9,17 +9,26 @@ from models import Base
 #SUPABASE_URL = os.getenv("SUPABASE_URL")
 #SUPABASE_PASSWORD = os.getenv("SUPABASE_PASSWORD")
 
-SUPABASE_URL = st.secrets["supabase"]["url"]
-SUPABASE_PASSWORD = st.secrets["supabase"]["password"]
+#SUPABASE_URL = st.secrets["supabase"]["url"]
+#SUPABASE_PASSWORD = st.secrets["supabase"]["password"]
 
-if "db." not in SUPABASE_URL:
-    SUPABASE_URL = f"db.{SUPABASE_URL}"
+#if "db." not in SUPABASE_URL:
+#    SUPABASE_URL = f"db.{SUPABASE_URL}"
 
-DATABASE_URL = f"postgresql://postgres:{SUPABASE_PASSWORD}@{SUPABASE_URL}:5432/postgres"
+#DATABASE_URL = f"postgresql://postgres:{SUPABASE_PASSWORD}@{SUPABASE_URL}:5432/postgres"
+
+from supabase import create_client
+
+url = st.secrets["SUPABASE_URL"]
+key = st.secrets["SUPABASE_KEY"]
+supabase = create_client(url, key)
+
+response = supabase.table("games").select("*").limit(1).execute()
+st.write(response.data)
 
 
-engine = create_engine(DATABASE_URL, echo=True)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+#engine = create_engine(DATABASE_URL, echo=True)
+#SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-def init_db():
-    Base.metadata.create_all(bind=engine)
+#def init_db():
+#    Base.metadata.create_all(bind=engine)
