425fcbd32b85ba0f1d30cfa6dcd80cf875ac3845
Marvin <marbachmann90@gmail.com>
2025-04-06 22:47:39 +0200

    add yvy

--- test-app.py
+++ test-app.py
@@ -7,7 +7,7 @@ supabase = create_client(
 )
 
 try:
-    response = supabase.table("players").insert({"name": "Marvin"}).execute()
+    response = supabase.table("players").insert({"name": "Yvy"}).execute()
     st.write(f"Insert successful: {response}")
 except Exception as e:
     st.write(f"An error occurred: {e}")
