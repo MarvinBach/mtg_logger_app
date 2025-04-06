0f11f2c06ea343671ae0fce7ee688a1b006eceb4
Marvin <marbachmann90@gmail.com>
2025-04-06 17:12:54 +0200

    update database url

--- db.py
+++ db.py
@@ -1,11 +1,16 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from models import Base
 
-SUPABASE_URL = "ncjplkroyenlrsuhkevf.supabase.co"
-SUPABASE_PASSWORD = "asflkj897Add!"
+# SUPABASE_URL = "ncjplkroyenlrsuhkevf.supabase.co"
+# SUPABASE_PASSWORD = "asflkj897Add!"
+SUPABASE_URL = os.environ.get("SUPABASE_URL")
+SUPABASE_PASSWORD = os.environ.get("SUPABASE_PASSWORD")
 
-DATABASE_URL = f"postgresql://postgres:{SUPABASE_PASSWORD}@db.{SUPABASE_URL}:5432/postgres"
+# DATABASE_URL = f"postgresql://postgres:{SUPABASE_PASSWORD}@db.{SUPABASE_URL}:5432/postgres"
+DATABASE_URL = f"postgresql://postgres:{SUPABASE_PASSWORD}@{SUPABASE_URL}:5432/postgres"
+
 
 engine = create_engine(DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
