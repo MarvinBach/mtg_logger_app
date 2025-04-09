3bfcd53958a33b0f68d025cbad6242a69c1ec6b5
Marvin <marbachmann90@gmail.com>
2025-04-09 22:24:30 +0200

    update add player

--- logger.py
+++ logger.py
@@ -2,10 +2,15 @@ from models import Player
 
 
 def add_player(name: str) -> str:
-    """Adds a new player to the database."""
+    """Adds a new player to the database"""
     name = name.strip()
     if not name:
         raise ValueError("Player name cannot be empty.")
 
-    Player.add(name=name)
+    player = Player(name=name)
+    response = player.add()
+
+    if response.status_code != 201:
+        raise Exception(f"Supabase error: {response}")
+
     return f"Player {name} added successfully!"
