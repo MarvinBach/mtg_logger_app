643ae334e91c20f534b4cd78cf8602c599ac7f5f
Marvin <marbachmann90@gmail.com>
2025-04-09 10:28:09 +0200

    moved add_game

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import pandas as pd
 from datetime import datetime
 
 from constants import EDITION_OPTIONS, COLOR_OPTIONS, FORMAT_OPTIONS
+from db import add_game
 from models import Player, Game
 from plot import WinRatePlotter
 
@@ -11,47 +12,7 @@ st.title("Magic The Gathering Game Logger")
 
 # --- Add Game Result ---
 st.header("Add game result")
-players = Player.get_all()
-player_map = {p["name"]: p["id"] for p in players}
-player_names = list(player_map.keys())
-
-if not player_names:
-    st.warning("Add some players first!")
-else:
-    winner = st.selectbox("Winner", player_names, key="winner")
-    loser = st.selectbox("Loser", player_names, key="loser")
-    game_format = st.selectbox(
-        "Format", ["Draft", "Sealed", "Cube Draft", "Constructed", "Commander", "Arena"]
-    )
-    selected_edition = st.selectbox(
-        "Edition (optional)", options=["None"] + EDITION_OPTIONS, index=0
-    )
-    winner_colors = st.multiselect(
-        "Winner's Colors (optional)", options=COLOR_OPTIONS, default=[]
-    )
-    loser_colors = st.multiselect(
-        "Loser's Colors (optional)", options=COLOR_OPTIONS, default=[]
-    )
-
-    if st.button("Add Game Result"):
-        if winner == loser:
-            st.error("Winner and loser cannot be the same player.")
-        else:
-            edition_to_submit = selected_edition if selected_edition != "None" else None
-            try:
-                Game(
-                    winner_id=player_map[winner],
-                    loser_id=player_map[loser],
-                    game_format=game_format,
-                    edition=edition_to_submit,
-                    winner_colors=winner_colors,
-                    loser_colors=loser_colors,
-                ).add()
-                st.success(
-                    f"Game result added: {winner} defeated {loser} in {game_format} format!"
-                )
-            except Exception as e:
-                st.error(f"Failed to add game: {e}")
+add_game()
 
 # --- Game History ---
 st.header("Game History")
---
+++ logger.py
@@ -0,0 +1,51 @@
+import streamlit as st
+
+from models import Player, Game
+from constants import EDITION_OPTIONS, COLOR_OPTIONS
+
+
+def add_game() -> None:
+    players = Player.get_all()
+    player_map = {p["name"]: p["id"] for p in players}
+    player_names = list(player_map.keys())
+
+    if not player_names:
+        st.warning("Add some players first!")
+    else:
+        winner = st.selectbox("Winner", player_names, key="winner")
+        loser = st.selectbox("Loser", player_names, key="loser")
+        game_format = st.selectbox(
+            "Format",
+            ["Draft", "Sealed", "Cube Draft", "Constructed", "Commander", "Arena"],
+        )
+        selected_edition = st.selectbox(
+            "Edition (optional)", options=["None"] + EDITION_OPTIONS, index=0
+        )
+        winner_colors = st.multiselect(
+            "Winner's Colors (optional)", options=COLOR_OPTIONS, default=[]
+        )
+        loser_colors = st.multiselect(
+            "Loser's Colors (optional)", options=COLOR_OPTIONS, default=[]
+        )
+
+        if st.button("Add Game Result"):
+            if winner == loser:
+                st.error("Winner and loser cannot be the same player.")
+            else:
+                edition_to_submit = (
+                    selected_edition if selected_edition != "None" else None
+                )
+                try:
+                    Game(
+                        winner_id=player_map[winner],
+                        loser_id=player_map[loser],
+                        game_format=game_format,
+                        edition=edition_to_submit,
+                        winner_colors=winner_colors,
+                        loser_colors=loser_colors,
+                    ).add()
+                    st.success(
+                        f"Game result added: {winner} defeated {loser} in {game_format} format!"
+                    )
+                except Exception as e:
+                    st.error(f"Failed to add game: {e}")
